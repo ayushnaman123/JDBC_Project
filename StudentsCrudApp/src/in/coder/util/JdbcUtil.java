e5ee1a32689a2516aa4d04b614cb48ebf0a2e28a
ayushnaman123 <ayushnaman808@gmail.com>
2024-09-08 23:00:21 +0530

    with HikariCp Connection Pooling

--- StudentsCrudApp/src/in/coder/util/JdbcUtil.java
+++ StudentsCrudApp/src/in/coder/util/JdbcUtil.java
@@ -1,37 +1,33 @@
 package in.coder.util;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Properties;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 public class JdbcUtil {
 	private JdbcUtil() {
 	}
-
+    
+	static {
+		// Step1: loading and register the Driver
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException ce) {
+			ce.printStackTrace();
+		}
+	}
+	
 	public static Connection getJdbcConnection() throws IOException, SQLException {
-		FileInputStream fis = new FileInputStream("src\\in\\coder\\properties\\Application.properties");
-		Properties properties = new Properties();
-		properties.load(fis);
-
-		Connection connection = DriverManager.getConnection(properties.getProperty("url"),
-				properties.getProperty("username"), properties.getProperty("password"));
+		
+        String configFile = "src\\in\\coder\\properties\\Application.properties";
+		HikariConfig config = new HikariConfig(configFile);
+		HikariDataSource dataSource = new HikariDataSource(config);
+		Connection connection = dataSource.getConnection();
 		return connection;
 	}
 
-	public static void cleanUp(Connection connection, Statement statement, ResultSet resultSet) throws SQLException {
-		if (resultSet != null)
-			resultSet.close();
-
-		if (statement != null)
-			statement.close();
-
-		if (connection != null)
-			connection.close();
-
-	}
+	
 }
